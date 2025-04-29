814fdc285fd142511ff8e3e96685595d420b7dfe
vijay <vijay49m@gmail.com>
2025-04-29 08:16:00 +0530

    ecs-provisioning

---
+++ ecs.tf
@@ -0,0 +1,11 @@
+module "ecs" {
+  source          = "git::https://github.com/vijaya49/Terraform-Modules.git//vpc?ref=main"
+
+  app_name        = "prod-ecs"
+  container_port  = 80
+  image_tag       = var.image_tag
+  domain_name     = "production.cloudvj.in"
+  vpc_id          = "vpc-0f67aaef49b800c43"
+  public_subnet_ids      = ["subnet-0f640e4b87bc6bee5", "subnet-0af195a3b64176b82"]
+  private_subnet_ids      = ["subnet-06d703c919fa37976", "subnet-06b62a8e43cc8704e"]
+}
---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "ecs_cluster_id" {
+  description = "The ID of the ECS Cluster"
+  value       = module.ecs.ecs_cluster_id
+}
+
+output "ecs_service_name" {
+  description = "The name of the ECS Service"
+  value       = module.ecs.ecs_service_name
+}
+
+output "task_definition_arn" {
+  description = "The ARN of the ECS Task Definition"
+  value       = module.ecs.task_definition_arn
+}
+
+output "load_balancer_dns_name" {
+  description = "The DNS name of the Load Balancer"
+  value       = module.ecs.load_balancer_dns_name
+}
+
+output "target_group_arn" {
+  description = "The ARN of the Target Group"
+  value       = module.ecs.target_group_arn
+}
